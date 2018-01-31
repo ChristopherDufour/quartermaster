a987b2a1bb884765af1079a2d58903e5a4bd4304
Chris Dufour <chris@cdufour.com>
2018-01-30 22:09:46 -0500

    Add material UI, update title text to toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <p>Quartermaster</p>
+        <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography type="title" color="inherit">
+            Quartermaster
+          </Typography>
+        </Toolbar>
+      </AppBar>
       </div>
     );
   }
